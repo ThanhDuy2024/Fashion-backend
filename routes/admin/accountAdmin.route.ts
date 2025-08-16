28e27266a783f39dfeb2ce9f347d699fd600b9fb
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-08-16 14:34:57 +0700

    [Feature] validate register api

--- routes/admin/accountAdmin.route.ts
+++ routes/admin/accountAdmin.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import * as accountAdminController from "../../controllers/admin/accountAdmin.controller";
+import * as validate from "../../validates/auth.validate";
 const route = Router();
 
-route.post("/register", accountAdminController.register);
+route.post("/register", validate.registerValidate, accountAdminController.register);
 
 export default route;
---
+++ validates/auth.validate.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi"
+export const registerValidate = (req: Request, res: Response, next: NextFunction) => {
+  const schema = Joi.object({
+    fullName: Joi.string().min(5).max(50).required()
+      .messages({
+        "string.empty": "Your name is blank!",
+        "string.min": "Your name must be at least 5 character long!",
+        "string.max": "Your name is limited to 50 characters only"
+      }),
+    email: Joi.string().email().required()
+      .messages({
+        "string.empty": "Your email is blank!",
+        "string.email": "Your email is broken!"
+      }),
+    password: Joi.string().min(5).max(50).required()
+      .messages({
+        "string.empty": "Your password is blank!",
+        "string.min": "Your password must be at least 5 character long!",
+        "string.max": "Your password is limited to 50 character only!"
+      })
+  })
+
+  const { error } = schema.validate(req.body);
+
+  if(error) {
+    res.status(400).json({
+      code: "error",
+      message: error.details[0].message
+    });
+    return;
+  }
+  next();
+}
