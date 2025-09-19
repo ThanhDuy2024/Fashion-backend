fe336e5ac2309fa5374fc0fd035e3ca177658502
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-09-19 15:09:56 +0700

    [Update] remove data not necessary

--- controllers/clients/categories.controller.ts
+++ controllers/clients/categories.controller.ts
@@ -24,11 +24,7 @@ export const categoriesTree = async (req: Request, res: Response) => {
     const rawData: any = {
       id: item.id,
       name: item.name,
-      image: item.image ? item.image : "",
       parentCategoryId: [],
-      status: item.status,
-      createdAtFormat: "",
-      updatedAtFormat: "",
     }
 
     for (const parent of item.parentCategoryId) {
@@ -42,9 +38,6 @@ export const categoriesTree = async (req: Request, res: Response) => {
         rawData.parentCategoryId.push(check.id);
       }
     }
-
-    rawData.createdAtFormat = moment(item.createdAt).format("HH:mm DD/MM/YYYY");
-    rawData.updatedAtFormat = moment(item.updatedAt).format("HH:mm DD/MM/YYYY");
 
     data.push(rawData);
   }
