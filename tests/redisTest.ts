0f6705285a6557b4cd109d2acafdab3a90ae42ba
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-11-20 16:04:27 +0700

    [Fix] redis is  not connected

--- tests/redisTest.ts
+++ tests/redisTest.ts
@@ -1,11 +1,20 @@
 import { createClient } from 'redis';
+import 'dotenv/config'
+const client = createClient({
+    username: process.env.REDIS_USER_NAME,
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: String(process.env.REDIS_URL),
+        port: Number(process.env.REDIS_PORT)
+    }
+});
 
 export const connectRedis = async () => {
-  const client = createClient({
-    url: "redis://alice:foobared@awesome.redis.server:6380"
-  });
-
   client.on('error', err => console.log('Redis Client Error', err));
-
+  
   await client.connect();
+
+  console.log("redis has been connect");
 }
+
+export default client;
