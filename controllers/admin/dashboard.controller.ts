a178196e78ab287e9fd03cc6172f087b3eb48b34
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-11-24 20:39:50 +0700

    [Update] total order on month

--- controllers/admin/dashboard.controller.ts
+++ controllers/admin/dashboard.controller.ts
@@ -1,11 +1,87 @@
 import { Response } from "express";
 import { admin } from "../../interface/admin.interface";
-
-export const dashboard = (req: admin, res: Response) => {
+import { Order } from "../../models/order.model";
+import moment, { months } from "moment";
+const currentYear = new Date().getFullYear();
+export const dashboardOrder = async (req: admin, res: Response) => {
   try {
+    //Lay ra tong don hang co trong thang
+    const order = await Order.find({
+      createdAt: {
+        $gte: new Date(`${currentYear}-01-01`),
+        $lte: new Date(`${currentYear}-12-31`)
+      }
+    }).select({
+      id: 1,
+      userId: 1,
+      createdAt: 1
+    });
+
+    const orderMonth: any = [
+      {
+        month: "01",
+        totalOrder: 0
+      },
+      {
+        month: "02",
+        totalOrder: 0
+      },
+      {
+        month: "03",
+        totalOrder: 0
+      },
+      {
+        month: "04",
+        totalOrder: 0
+      },
+      {
+        month: "05",
+        totalOrder: 0
+      },
+      {
+        month: "06",
+        totalOrder: 0
+      },
+      {
+        month: "07",
+        totalOrder: 0
+      },
+      {
+        month: "08",
+        totalOrder: 0
+      },
+      {
+        month: "09",
+        totalOrder: 0
+      },
+      {
+        month: "10",
+        totalOrder: 0
+      },
+      {
+        month: "11",
+        totalOrder: 0
+      },
+      {
+        month: "12",
+        totalOrder: 0
+      }
+    ]
+
+    for (const om of orderMonth) {
+      for (const item of order) {
+        const my = moment(item.createdAt).format("YYYY-MM");
+        if (my === `${currentYear}-${om.month}`) {
+          om.totalOrder += 1
+        }
+      }
+    }
+
+    console.log(orderMonth);
+
     res.json({
       code: "success",
-      message: "dashboar api"
+      data: orderMonth
     })
   } catch (error) {
     console.log(error);
--- routes/admin/dashboard.route.ts
+++ routes/admin/dashboard.route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import * as dashboardController from "../../controllers/admin/dashboard.controller"
 const route = Router();
 
-route.get("/list", dashboardController.dashboard);
+route.get("/list", dashboardController.dashboardOrder);
 
 export default route;
